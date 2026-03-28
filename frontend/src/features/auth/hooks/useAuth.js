380bd1fa97ed066197f8a11a27cd98a25f7ceefb
Mirza Isfand <mirzaisfand321@gmail.com>
2026-03-29 01:06:47 +0530

    custom hook(useAuth) & two way binding

---
+++ frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,58 @@
+import {useContext} from 'react';
+import {AuthContext} from '../auth.context';
+import {login, register, logout, getMe} from '../services/auth.api';
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  const {user , setUser , loading , setLoading} = context;
+
+  const handleLogin = async (email, password) => {
+    setLoading(true);
+    try {
+      const userData = await login(email, password);
+      setUser(userData);
+    } catch (error) {
+      console.error('Login failed:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleRegister = async (username ,email, password, name) => {
+    setLoading(true);
+    try {
+      const userData = await register(username, email, password, name);
+      setUser(userData);
+    } catch (error) {
+      console.error('Registration failed:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleLogout = async () => {
+    setLoading(true);
+    try {
+      await logout();
+      setUser(null);
+    } catch (error) {
+      console.error('Logout failed:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchUserProfile = async () => {
+    setLoading(true);
+    try {
+      const userData = await getMe();
+      setUser(userData);
+    } catch (error) {
+      console.error('Failed to fetch user profile:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { user, loading, handleLogin, handleRegister, handleLogout, fetchUserProfile };
+};
