9337776fa91e4e154c3176882ff9bac3692a8aa0
Mirza Isfand <mirzaisfand321@gmail.com>
2026-03-30 14:13:18 +0530

    add home component and update routing

--- frontend/src/features/auth/hooks/useAuth.js
+++ frontend/src/features/auth/hooks/useAuth.js
@@ -1,69 +1,66 @@
-import {useContext , useEffect} from 'react';
-import {AuthContext} from '../auth.context';
-import {login, register, logout, getMe} from '../services/auth.api';
+import { useContext, useEffect } from "react";
+import { AuthContext } from "../auth.context";
+import { login, register, logout, getMe } from "../services/auth.api";
+
+
 
 export const useAuth = () => {
-  const context = useContext(AuthContext);
-  const {user , setUser , loading , setLoading} = context;
 
-  const handleLogin = async (email, password) => {
-    setLoading(true);
-    try {
-      const userData = await login(email, password);
-      setUser(userData);
-    } catch (error) {
-      console.error('Login failed:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
+    const context = useContext(AuthContext)
+    const { user, setUser, loading, setLoading } = context
 
-  const handleRegister = async (username ,email, password, name) => {
-    setLoading(true);
-    try {
-      const userData = await register(username, email, password, name);
-      setUser(userData);
-    } catch (error) {
-      console.error('Registration failed:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
 
-  const handleLogout = async () => {
-    setLoading(true);
-    try {
-      await logout();
-      setUser(null);
-    } catch (error) {
-      console.error('Logout failed:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
+    const handleLogin = async ({ email, password }) => {
+        setLoading(true)
+        try {
+            const data = await login({ email, password })
+            setUser(data.user)
+        } catch (err) {
 
-  const fetchUserProfile = async () => {
-    setLoading(true);
-    try {
-      const userData = await getMe();
-      setUser(userData);
-    } catch (error) {
-      console.error('Failed to fetch user profile:', error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-      useEffect(() => {
-        const fetchUser = async () => {
-         const data = await getMe()
-         setUser(data)
-         setLoading(false)
+        } finally {
+            setLoading(false)
         }
-        fetchUser()
     }
-    ,[])
 
+    const handleRegister = async ({ username, email, password }) => {
+        setLoading(true)
+        try {
+            const data = await register({ username, email, password })
+            setUser(data.user)
+        } catch (err) {
 
-  return { user, loading, handleLogin, handleRegister, handleLogout, fetchUserProfile };
-};
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const handleLogout = async () => {
+        setLoading(true)
+        try {
+            const data = await logout()
+            setUser(null)
+        } catch (err) {
+
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+
+        const getAndSetUser = async () => {
+            try {
+
+                const data = await getMe()
+                setUser(data.user)
+            } catch (err) { } finally {
+                setLoading(false)
+            }
+        }
+
+        getAndSetUser()
+
+    }, [])
+
+    return { user, loading, handleRegister, handleLogin, handleLogout }
+}
