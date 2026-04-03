3245b7e7f66b8e9b311e4b9eeaa85f76c0672c47
Mirza Isfand <mirzaisfand321@gmail.com>
2026-04-03 19:24:19 +0530

    lamest error fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import { RouterProvider } from "react-router"
 import { router } from "./app.routes.jsx"
-import { AuthProvider } from "./features/auth/auth.context.jsx"
-import { InterviewProvider } from "./features/interview/interview.provider"
 import { AuthProvider } from "./features/auth/auth.provider"
+import { InterviewProvider } from "./features/interview/interview.provider"
 
 function App() {
-
   return (
     <AuthProvider>
       <InterviewProvider>
