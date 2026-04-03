63c0a4f3c99bf1003d1598dacb510a50f5b44b5c
Mirza Isfand <mirzaisfand321@gmail.com>
2026-04-03 16:38:54 +0530

    minor update

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,9 @@ const app = require("./src/app");
 const connectToDB = require("./src/config/database");
 
 connectToDB();
+const port = process.env.PORT || 3000;
 
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
